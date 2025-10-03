5275cd5da5b33224787518e4cce313e548b7cbb5
Ho Cong Toan <hocongtoan987@gmail.com>
2025-10-03 18:51:19 +0700

    fix param

--- src/main/java/org/toanehihi/jobrecruitmentplatformserver/interfaces/web/controllers/job/JobController.java
+++ src/main/java/org/toanehihi/jobrecruitmentplatformserver/interfaces/web/controllers/job/JobController.java
@@ -1,8 +1,10 @@
 package org.toanehihi.jobrecruitmentplatformserver.interfaces.web.controllers.job;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.toanehihi.jobrecruitmentplatformserver.application.job.service.JobService;
 import org.toanehihi.jobrecruitmentplatformserver.interfaces.web.dtos.PageResult;
@@ -15,7 +17,11 @@ public class JobController {
     private final JobService jobService;
 
     @GetMapping
-    public PageResult<JobResponse> getAllJobs(int page, int size, String sortBy, String sortDir) {
+    public PageResult<JobResponse> getAllJobs(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDir) {
         return jobService.getAllJobs(page, size, sortBy, sortDir);
     }
 
