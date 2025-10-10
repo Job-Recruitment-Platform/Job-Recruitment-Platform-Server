28cf398571b18cb2497f38b8d77d0b2962411b64
Lezh1n <hoangphilong1208@gmail.com>
2025-10-10 18:30:43 +0700

    chore: move files

--- src/main/java/org/toanehihi/jobrecruitmentplatformserver/application/email/service/EmailServiceImpl.java
+++ src/main/java/org/toanehihi/jobrecruitmentplatformserver/application/email/service/EmailServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.toanehihi.jobrecruitmentplatformserver.application.email.service.EmailService;
 import org.toanehihi.jobrecruitmentplatformserver.domain.exception.AppException;
 import org.toanehihi.jobrecruitmentplatformserver.domain.exception.ErrorCode;
 
